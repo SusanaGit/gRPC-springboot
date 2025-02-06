8899949455e7a13cdeddba15f4d547205b4efc68
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2025-02-06 21:38:37 +0100

    implement tradeStock method of the UserService GrpcService

--- user-service/src/main/java/com/susanafigueroa/userservice/service/UserService.java
+++ user-service/src/main/java/com/susanafigueroa/userservice/service/UserService.java
@@ -1,18 +1,24 @@
 package com.susanafigueroa.userservice.service;
 
 import com.susanafigueroa.user.*;
+import com.susanafigueroa.userservice.service.handler.StockTradeRequestHandler;
 import com.susanafigueroa.userservice.service.handler.UserInformationRequestHandler;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 // permite que Spring detecte e inyecte el servicio automáticamente en el servidor gRPC
 @GrpcService
 public class UserService extends UserServiceGrpc.UserServiceImplBase {
 
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
     private final UserInformationRequestHandler userInformationRequestHandler;
+    private final StockTradeRequestHandler stockTradeRequestHandler;
 
-    public UserService(UserInformationRequestHandler userInformationRequestHandler) {
+    public UserService(UserInformationRequestHandler userInformationRequestHandler, StockTradeRequestHandler stockTradeRequestHandler) {
         this.userInformationRequestHandler = userInformationRequestHandler;
+        this.stockTradeRequestHandler = stockTradeRequestHandler;
     }
 
     // cuando el cliente envía la request, aquí es donde se recibe
@@ -31,5 +37,11 @@ public class UserService extends UserServiceGrpc.UserServiceImplBase {
 
     @Override
     public void tradeStock(StockTradeRequest request, StreamObserver<StockTradeResponse> responseObserver) {
+        log.info("{}", request);
+        var response = TradeAction.SELL.equals(request.getAction()) ?
+                this.stockTradeRequestHandler.buyStock(request) :
+                this.stockTradeRequestHandler.sellStock(request);
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 }
